9482feab5c65c2a9bd3eccd24f3c2fbe98ea10cc
clvvh <duan000310@gmail.com>
2020-09-07 22:45:42 +0800

    Factorization

---
+++ Factorization/Factorization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Factorization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
