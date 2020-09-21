20bdee3abfb52a8bb6900bd731e3f28972ea916a
clvvh <duan000310@gmail.com>
2020-09-21 22:17:21 +0800

    hwk2_Clock

---
+++ test/test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object a=null;
+            try
+            {
+                int b = (int)a;
+            }catch(InvalidCastException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("asdaasd");
+        }
+
+    }
+}
